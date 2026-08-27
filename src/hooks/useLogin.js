15856aa06ddb76f82f7f25a9f8c2f2498df19f56
Morgane Bourdillon <mlacroix8@gmail.com>
2026-08-27 14:13:47 +0200

    set up remember me

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,12 +1,18 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { loginAction } from "../store/auth/authSlice";
 
 const useLogin = () => {
   // Récupération du dispatch pour mettre à jour l'état global
   const dispatch = useDispatch();
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   // Fonction pour se connecter avec POST
-  const login = async (username, password) => {
+  const login = async (username, password, rememberMe) => {
+    setIsLoading(true);
+    setError(null);
     const response = await fetch("http://localhost:3001/api/v1/user/login", {
     method: "POST",
     headers: {
@@ -20,12 +26,24 @@ const useLogin = () => {
 
   // Si la requête est réussie, on met à jour l'état global avec le token
   if (response.ok) {
-    dispatch(loginAction({ token: data.body.token }));
+    // Stockage du token dans le localStorage
+     if (rememberMe) {
+        localStorage.setItem("token", data.body.token);
+      } else {
+        sessionStorage.setItem("token", data.body.token);
+      }
+
+      dispatch(loginAction({ token: data.body.token }));
+    setIsLoading(false);
+
+
     return true;
   }
+  setIsLoading(false);
+  setError("Invalid username or password");
   return false;
 }
 
-return { login };
+return { login, isLoading, error };
 }
 export default useLogin;
